05e0381a0c94a128f3595a158310b98c8bf511ee
dominik987654 <dominik694@o2.pl>
2018-01-18 13:29:30 +0100

    gitest

---
+++ CPP/ul_ulamek.cpp
@@ -0,0 +1,49 @@
+/*
+ */
+
+
+#include <iostream>
+#include "ul_ulamek.h"
+
+using namespace std;
+
+
+Ulamek::Ulamek(int l, int m){
+    licznik = l;
+    if(m!=0) mianownik = m;
+    else {
+        cout << "Nie dziel przez zero!";
+        exit(1);        
+    }
+}
+
+void Ulamek::zapisz(int l, int m){
+    licznik = l;
+    if(m!=0) mianownik = m;
+    else {
+        cout << "Nie dziel przez zero!";
+        exit(1);        
+    }
+}
+
+
+void Ulamek::wypisz(){
+    cout << licznik << "/" << mianownik;
+}
+
+int Ulamek::get_l(){
+    return licznik;
+}
+
+int Ulamek::get_m(){
+    return mianownik;
+}
+
+
+
+void Ulamek::skracaj(){
+    ; //algorytm euklidesa
+}
+
+
+
---
+++ CPP/ul_ulamek.h
@@ -0,0 +1,17 @@
+/*
+* plik naglowkowy klasy ulamek
+ */
+
+
+class Ulamek{
+private:
+    int licznik;   //deklaracja skladowej wlasciwosci
+    int mianownik;
+public:
+    Ulamek(int, int); //deklaracja konstruktora     
+    void zapisz(int, int);  //deklaracja metody nie trzeba podawac zmiennych
+    void wypisz();
+    int get_l();
+    int get_m();
+    void skracaj();  //drukuje skrocona postac ulamka
+};
