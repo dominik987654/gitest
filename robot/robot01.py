4a58f944f358e0df4489da7eb27a4dcae9ebc1df
dominik987654 <dominik694@o2.pl>
2018-03-06 14:20:33 +0100

    gitest

--- robot/robot01.py
+++ robot/robot01.py
@@ -6,28 +6,39 @@ import rg
 
 class Robot:
 
+
+
     def act(self, game):
-        ilu_wrogow = 0
-        lista_wrogow = []
+
+        def czy_wejscie(poz):
+            if 'spawn' in rg.loc_types(poz):
+                return True
+            return False
+
+
+
+             # ilu_wrogow = 0
+        lista_wrogow_obok = []
 
         for poz, robot in game.robots.iteritems():
-            if robot.player_id != self.player_id:
-                 if rg.dist(poz, self.location) <= 1:
-                    lista_wrogow.append(poz)
-                      #return ['attack', poz]
-        if len(lista_wrogow) > 2:
-            return['suicide']
+            if robot.player_id != self.player_id:           #rozpoznanie wroga
+                if rg.dist(poz, self.location) <= 1:
+                    lista_wrogow_obok.append(poz)
+                    # ilu_wrogow += 1
+                    # return ['attack', poz]
+        print(lista_wrogow_obok)
 
-        elif len(lista_wrogow):
-            return ['attack', lista_wrogow[0]]
+
+        if len(lista_wrogow_obok) > 2:
+            return['suicide']                                     #samobojstwo
+        elif len(lista_wrogow_obok):
+            return['attack', lista_wrogow[0]]                             #atak
 
         print(game.robots)
 
         if self.location == rg.CENTER_POINT:
-            return ['guard']
+            return ['guard']                                            #obrona
 
-
+        if czy_wejscie(self.poz):
         # idź do środka planszy, ruch domyślny
         return ['move', rg.toward(self.location, rg.CENTER_POINT)]
-
-
